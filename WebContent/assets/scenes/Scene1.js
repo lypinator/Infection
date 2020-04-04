d5e79a5130b3754984f98f983af778c0eba5036b
Andrew Lyp <LYPAND@chrobinson.com>
2020-04-04 01:26:44 -0500

    Change to use velocity rather than x y coordinates

--- WebContent/assets/scenes/Scene1.js
+++ WebContent/assets/scenes/Scene1.js
@@ -17,71 +17,71 @@ function itemCollisionHandler() {
 /* START OF COMPILED CODE */
 
 class Scene1 extends Phaser.Scene {
-	
-	constructor() {
-	
-		super("Scene1");
-		
-	}
-	
-	_create() {
-	
-		this.add.image(1000.0, 1000.0, "background1");
-		
-		var water = this.add.image(1610.7423, 1817.4121, "water");
-		water.setScale(2.5825047, 1.2230265);
-		
-		var platform = this.add.image(224.0, 912.0, "platform");
-		platform.setData("Health", 100);
-		
-		var platform_1 = this.add.image(222.07042, 1131.7639, "platform");
-		platform_1.setData("Health", 100);
-		
-		var platform_3 = this.add.image(296.66806, 998.15063, "platform");
-		platform_3.setData("body.immovable", true);
-		platform_3.setData("Health", 100);
-		platform_3.setScale(0.12639447, 4.9188023);
-		
-		var platform_2 = this.add.image(45.849155, 1022.5466, "platform");
-		platform_2.setData("Health", 100);
-		platform_2.setScale(0.10933112, 7.793609);
-		
-		var platform_4 = this.add.image(923.90283, 924.77264, "platform");
-		platform_4.setData("Health", 100);
-		
-		var platform_5 = this.add.image(1097.0784, 1099.6459, "platform");
-		platform_5.setData("Health", 100);
-		platform_5.setScale(0.11905486, 12.137681);
-		
-		var gunPickup = this.add.image(617.5271, 1040.9796, "gunPickup");
-		
-		var gameInfo = this.add.text(498.5, 313.0, "New text", {
-    "fontSize": "25px",
-    "color": "#FFFFFF",
-    "stroke": "#FF8080"
-});
-		gameInfo.visible = false;
-		
-		var player = this.add.sprite(491.7915, 1036.438, "WalkLeftStand-removebg-preview");
-		player.setData("hasGun", false);
-		player.setScale(0.48163477, 0.35892242);
-		player.anims.play("LeftWalkLeftStand-removebg-preview");
-		
-		this.fWalls = this.add.group([ platform, platform_3, platform_1, platform_2, platform_4, platform_5 ]);
-		this.fWater = this.add.group([ water ]);
-		
-		this.fGunPickup = gunPickup;
-		this.fGameInfo = gameInfo;
-		this.fPlayer = player;
-		
-	}
-	
-	
-	
-	
-	
-	
-	/* START-USER-CODE */
+
+    constructor() {
+
+        super("Scene1");
+
+    }
+
+    _create() {
+
+        this.add.image(1000.0, 1000.0, "background1");
+
+        var water = this.add.image(1610.7423, 1817.4121, "water");
+        water.setScale(2.5825047, 1.2230265);
+
+        var platform = this.add.image(224.0, 912.0, "platform");
+        platform.setData("Health", 100);
+
+        var platform_1 = this.add.image(222.07042, 1131.7639, "platform");
+        platform_1.setData("Health", 100);
+
+        var platform_3 = this.add.image(296.66806, 998.15063, "platform");
+        platform_3.setData("body.immovable", true);
+        platform_3.setData("Health", 100);
+        platform_3.setScale(0.12639447, 4.9188023);
+
+        var platform_2 = this.add.image(45.849155, 1022.5466, "platform");
+        platform_2.setData("Health", 100);
+        platform_2.setScale(0.10933112, 7.793609);
+
+        var platform_4 = this.add.image(923.90283, 924.77264, "platform");
+        platform_4.setData("Health", 100);
+
+        var platform_5 = this.add.image(1097.0784, 1099.6459, "platform");
+        platform_5.setData("Health", 100);
+        platform_5.setScale(0.11905486, 12.137681);
+
+        var gunPickup = this.add.image(617.5271, 1040.9796, "gunPickup");
+
+        var gameInfo = this.add.text(498.5, 313.0, "New text", {
+            "fontSize": "25px",
+            "color": "#FFFFFF",
+            "stroke": "#FF8080"
+        });
+        gameInfo.visible = false;
+
+        var player = this.add.sprite(491.7915, 1036.438, "WalkLeftStand-removebg-preview");
+        player.setData("hasGun", false);
+        player.setScale(0.48163477, 0.35892242);
+        player.anims.play("LeftWalkLeftStand-removebg-preview");
+
+        this.fWalls = this.add.group([platform, platform_3, platform_1, platform_2, platform_4, platform_5]);
+        this.fWater = this.add.group([water]);
+
+        this.fGunPickup = gunPickup;
+        this.fGameInfo = gameInfo;
+        this.fPlayer = player;
+
+    }
+
+
+
+
+
+
+    /* START-USER-CODE */
 
     create() {
         this.pickUpDuration = 0;
@@ -89,8 +89,16 @@ class Scene1 extends Phaser.Scene {
         this.activeWall = null;
         this.playerWallOffsetX;
         this.playerWallOffsetY;
+        this.sendUp = true;
+        this.sendDown = true;
+        this.sendRight = true;
+        this.sendLeft = true;
+        this.playerXVelocity = 0;
+        this.playerYVelocity = 0;
+        this.defaultVelocity = 200;
+
         this._create();
-        connectToServer(this); 
+        connectToServer(this);
         this.configurePlayer();
         this.configureItems();
         this.configureWalls();
@@ -101,52 +109,17 @@ class Scene1 extends Phaser.Scene {
 
     update() {
 
-        this.configureCollisionHandler(); 
+        this.configureCollisionHandler();
 
-        if (this.key_UP.isDown) {
-            this.fPlayer.body.velocity.y = -200;
-            directionY = -300;
-            directionX = 0;
-            this.sendPlayerUpdate(); 
-        }
-        if (this.key_LEFT.isDown) {
-            this.fPlayer.body.velocity.x = -200;
-            this.fPlayer.anims.play("LeftWalkLeftStand-removebg-preview", true);
-            directionX = -300;
-            directionY = 0;
-            this.sendPlayerUpdate(); 
-        }
-        if (this.key_DOWN.isDown) {
-            this.fPlayer.body.velocity.y = 200;
-            directionY = 300;
-            directionX = 0;
-            this.sendPlayerUpdate(); 
+        this.fPlayer.body.setVelocity(this.playerXVelocity, this.playerYVelocity);
 
-        }
-        if (this.key_RIGHT.isDown) {
-            this.fPlayer.body.velocity.x = 200;
-            directionX = 300;
-            directionY = 0;
-            this.sendPlayerUpdate(); 
-        }
+        this.handelUpKey();
+        this.handelDownKey();
+        this.handelLeftKey();
+        this.handelRightKey();
+        this.handelWallUpdate(); 
+        this.updateAllPlayers();
 
-        if (this.key_RIGHT.isUp && this.key_DOWN.isUp && this.key_LEFT.isUp && this.key_UP.isUp) {
-            this.fPlayer.body.velocity.y = 0;
-            this.fPlayer.body.velocity.x = 0;
-        }
-
-        if (this.key_LOCK.isDown && this.moveObject) {
-
-            this.activeWall.x = this.playerWallOffsetX + this.fPlayer.x;
-            this.activeWall.y = this.playerWallOffsetY + this.fPlayer.y;
-            this.sendWallUpdate(this.activeWall.body.name,this.activeWall.x, this.activeWall.y )
-
-        } else {
-            if (this.activeWall != null) {
-                this.activeWall.body.immovable = true;
-                this.moveObject = false;
-            }
-        }
 
         //Game information
         this.fGameInfo.x = this.fPlayer.x - 100;
@@ -154,7 +127,79 @@ class Scene1 extends Phaser.Scene {
 
         if (this.key_PICKUP.getDuration() > 1000 && this.isOverlapping(this.fPlayer, this.fGunPickup)) {
             this.fGunPickup.destroy();
-			this.fPlayer.setData('hasGun', true);
+            this.fPlayer.setData('hasGun', true);
+        }
+    }
+
+    handelWallUpdate(){
+        if (this.key_LOCK.isDown && this.moveObject) {
+
+            this.activeWall.x = this.playerWallOffsetX + this.fPlayer.x;
+            this.activeWall.y = this.playerWallOffsetY + this.fPlayer.y;
+            this.sendWallUpdate(this.activeWall.body.name, this.activeWall.x, this.activeWall.y)
+
+        } else {
+            if (this.activeWall != null) {
+                this.activeWall.body.immovable = true;
+                this.moveObject = false;
+            }
+        }
+    }
+
+    handelUpKey() {
+        if (this.key_UP.isDown && this.sendUp) {
+            this.playerYVelocity = -this.defaultVelocity;
+            this.sendPlayerUpdate();
+            this.sendUp = false;
+
+        }
+        if (this.key_UP.isUp && this.sendUp === false) {
+            this.playerYVelocity = this.key_DOWN.isDown ? this.defaultVelocity : 0; 
+            this.sendUp = true;
+            this.sendPlayerUpdate();
+        }
+    }
+    handelDownKey() {
+        if (this.key_DOWN.isDown && this.sendDown) {
+            this.playerYVelocity = this.defaultVelocity;
+            this.sendPlayerUpdate();
+            this.sendDown = false;
+        }
+        if (this.key_DOWN.isUp && this.sendDown === false) {
+            this.playerYVelocity = this.key_UP.isDown ? -this.defaultVelocity : 0;
+            this.sendDown = true;
+            this.sendPlayerUpdate();
+        }
+    }
+    handelRightKey() {
+
+        if (this.key_RIGHT.isDown && this.sendRight) {
+            this.playerXVelocity = this.defaultVelocity;
+            this.sendPlayerUpdate();
+            this.sendRight = false;
+        }
+        if (this.key_RIGHT.isUp && this.sendRight === false) {
+            this.playerXVelocity = this.key_LEFT.isDown ? -this.defaultVelocity : 0; 
+            this.sendRight = true;
+            this.sendPlayerUpdate();
+        }
+    }
+    handelLeftKey() {
+        if (this.key_LEFT.isDown && this.sendLeft) {
+            this.playerXVelocity = -this.defaultVelocity;
+            this.sendPlayerUpdate();
+            this.sendLeft = false;
+        }
+        if (this.key_LEFT.isUp && this.sendLeft === false) {
+            this.playerXVelocity = this.key_RIGHT.isDown ? this.defaultVelocity : 0; 
+            this.sendLeft = true;
+            this.sendPlayerUpdate();
+        }
+    }
+
+    updateAllPlayers() {
+        for (var [k, v] of allPlayers) {
+            v.player.body.setVelocity(v.xPos, v.yPos);
         }
     }
 
@@ -169,13 +214,13 @@ class Scene1 extends Phaser.Scene {
             this.playerWallOffsetY = wall.y - player.y;
         }
     }
-    sendPlayerUpdate(){
-        var updateMessage = { type: "MOVEMENT", playerId: playerId, xPos: this.fPlayer.x, yPos: this.fPlayer.y };
+    sendPlayerUpdate() {
+        var updateMessage = { type: "MOVEMENT", playerId: playerId, xPos: this.playerXVelocity, yPos: this.playerYVelocity };
         serverConnection.send(JSON.stringify(updateMessage));
     }
 
-    sendWallUpdate(wallId, xPos, yPos){
-        var updateMessage = { type: "WALLMOVEMENT",playerId: playerId, wallId: wallId, xPos: xPos, yPos: yPos };
+    sendWallUpdate(wallId, xPos, yPos) {
+        var updateMessage = { type: "WALLMOVEMENT", playerId: playerId, wallId: wallId, xPos: xPos, yPos: yPos };
         serverConnection.send(JSON.stringify(updateMessage));
     }
 
@@ -185,17 +230,17 @@ class Scene1 extends Phaser.Scene {
         return Phaser.Geom.Intersects.RectangleToRectangle(Phaser.Geom.Rectangle.Scale(spriteA, 1.5, 1.5).getBounds(), Phaser.Geom.Rectangle.Scale(spriteB, 1.5, 1.5).getBounds());
     }
 
-    configureKeyPressEvents(){
-    this.input.keyboard.on('keydown_SPACE', function (event) {
-        this.bullets.fireBullet(this.fPlayer.x, this.fPlayer.y, this.fPlayer.getData('hasGun'));
-    }, this);
-}
+    configureKeyPressEvents() {
+        this.input.keyboard.on('keydown_SPACE', function (event) {
+            this.bullets.fireBullet(this.fPlayer.x, this.fPlayer.y, this.fPlayer.getData('hasGun'));
+        }, this);
+    }
 
-configureCollisionHandler(){
-    this.physics.overlap(this.fPlayer, this.fGunPickup, itemCollisionHandler, null, this);
-    this.physics.world.addCollider(this.fPlayer, this.fWalls, this.wallCollisionHandler, null, this);
-	this.physics.world.addCollider(this.bullets, this.fWalls, this.wallShotHandler, null, this);
-}
+    configureCollisionHandler() {
+        this.physics.overlap(this.fPlayer, this.fGunPickup, itemCollisionHandler, null, this);
+        this.physics.world.addCollider(this.fPlayer, this.fWalls, this.wallCollisionHandler, null, this);
+        this.physics.world.addCollider(this.bullets, this.fWalls, this.wallShotHandler, null, this);
+    }
 
     configurePlayer() {
         this.physics.add.existing(this.fPlayer);
@@ -203,7 +248,6 @@ configureCollisionHandler(){
         this.fPlayer.body.setCollideWorldBounds(true);
         this.fPlayer.body.setDrag(2000);
         this.fPlayer.anims.duration = 1000;
-        this.fPlayer.body.setSize(this.fPlayer.width, this.fPlayer.height);
     }
     configureWalls() {
         var wallId = 0;
@@ -212,15 +256,15 @@ configureCollisionHandler(){
             wall.body.setSize(wall.width, wall.height);
             wall.body.setDrag(2000);
             wall.body.immovable = true;
-            wall.body.name = wallId; 
+            wall.body.name = wallId;
             wallId++;
         }
     }
 
-    initializeWalls(){
+    initializeWalls() {
         var wallId = 0;
         for (var wall of this.fWalls.children.entries) {
-            this.sendWallUpdate(wallId, wall.x, wall.y ); 
+            this.sendWallUpdate(wallId, wall.x, wall.y);
             wallId++;
         }
     }
@@ -236,23 +280,23 @@ configureCollisionHandler(){
         this.fGunPickup.body.setSize(this.fGunPickup.width, this.fGunPickup.height);
         this.fGunPickup.body.immovable = true;
     }
-	gunAdd(){
-		this.fGameInfo.text = "Hold E to Pickup Gun";
-    	this.fGameInfo.visible = true;
-    	this.time.removeAllEvents();
-    	this.time.delayedCall(100, hideText, [this.fGameInfo], this);
-	}
-	wallShotHandler(wall,bullet){
-		bullet.setActive(false);
+    gunAdd() {
+        this.fGameInfo.text = "Hold E to Pickup Gun";
+        this.fGameInfo.visible = true;
+        this.time.removeAllEvents();
+        this.time.delayedCall(100, hideText, [this.fGameInfo], this);
+    }
+    wallShotHandler(wall, bullet) {
+        bullet.setActive(false);
         bullet.setVisible(false);
-		var health = wall.getData('Health');
-		if (health - 10 <= 0){
-			wall.destroy();
-		}
-		else{
-		wall.setData('Health',health-10);
-		}
-	}
+        var health = wall.getData('Health');
+        if (health - 10 <= 0) {
+            wall.destroy();
+        }
+        else {
+            wall.setData('Health', health - 10);
+        }
+    }
     configureKeys() {
         this.key_UP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         this.key_LEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -263,7 +307,7 @@ configureCollisionHandler(){
         this.key_LOCK = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
     }
 
-/* END-USER-CODE */
+    /* END-USER-CODE */
 }
 
 /* END OF COMPILED CODE */
--- WebContent/serverConnection.js
+++ WebContent/serverConnection.js
@@ -16,7 +16,13 @@ function connectToServer(canvas) {
                 newPlayer.setData("hasGun", false);
                 newPlayer.setScale(0.48163477, 0.35892242);
                 newPlayer.anims.play("LeftWalkLeftStand-removebg-preview");
-                allPlayers.set(json.player.currentPlayerInformation.playerId, newPlayer);
+                canvas.physics.add.existing(newPlayer);
+                newPlayer.body.setCollideWorldBounds(true);
+                newPlayer.body.setDrag(2000);
+                newPlayer.anims.duration = 1000;
+                newPlayer.body.setSize(newPlayer.width, newPlayer.height);
+                allPlayers.set(json.player.currentPlayerInformation.playerId, {player: newPlayer, xPos: 0, yPos: 0});
+               
                 break;
             case 'CONNECTION_INFORMATION':
                 playerId = json.playerId;
@@ -25,14 +31,19 @@ function connectToServer(canvas) {
                     newPlayer1.setData("hasGun", false);
                     newPlayer1.setScale(0.48163477, 0.35892242);
                     newPlayer1.anims.play("LeftWalkLeftStand-removebg-preview");
-                    allPlayers.set(player.playerId, newPlayer1);
+                    canvas.physics.add.existing(newPlayer1);
+                    newPlayer1.body.setCollideWorldBounds(true);
+                    newPlayer1.body.setDrag(2000);
+                    newPlayer1.anims.duration = 1000;
+                    newPlayer1.body.setSize(newPlayer1.width, newPlayer1.height);
+                    allPlayers.set(player.playerId,{player: newPlayer1, xPos: player.xPos, yPos: player.yPos});
                     canvas.initializeWalls(); 
                 }
                 break;
             case 'PLAYER_MOVEMENT_UPDATE':
-                
-                allPlayers.get(json.playerId).x = json.xPos;
-                allPlayers.get(json.playerId).y = json.yPos;
+ 
+                allPlayers.get(json.playerId).xPos = json.xPos;
+                allPlayers.get(json.playerId).yPos = json.yPos;
                 break;
             case 'WALL_MOVEMENT_UPDATE':
                 canvas.fWalls.children.entries[json.wallId].x = json.xPos;
